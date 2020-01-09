8d150b8b5b67065d55e09869da55a8d3423c07ff
Ngochuy <ngngochuy1107@gmail.com>
2020-01-09 15:30:44 +0700

    Leave room function done

--- app.js
+++ app.js
@@ -8,30 +8,17 @@ app.get('/',function(req,res){
 var Userlist={};
 var ID={};
 var room={};
-//iosocket-part
-// io.on('connection',function(socket){
-//     var name;
-//     socket.on('disconnect',function(msg){
-//         if(name="") name="unnamed";
-//         io.emit('userleft',name);
-//     })
-//     socket.on('chat message',function(msg){
-//         io.emit('chat message',msg)
-//         name=msg.name;
-//     })
-// })
-
 //altenative iosocket
 io.on('connection',function(socket){
     socket.on('NewUser',function(username,id,roomid){
         socket.username=username;
         Userlist[username]=username;
         ID[username]=id;
-        room[username]=roomid;
         socket.roomid=roomid;
+        room[username]=roomid;
         socket.join(roomid);
         socket.emit('notify','You have joined chatroom ' +roomid);
-        socket.broadcast.to(socket.roomid).emit('notify',username+' have connected!');
+        socket.broadcast.to(socket.roomid).emit('notify',socket.username+' have connected!');
         var local={};
         for(people in Userlist){
             if(room[people]==socket.roomid)  local[people]=people;
@@ -42,9 +29,25 @@ io.on('connection',function(socket){
     socket.on('sendmes',function(data){
         io.to(socket.roomid).emit('post',socket.username,"      "+data);
     })
+    socket.on('leave-room',function(roomid){
+        socket.leave(socket.roomid);
+        socket.emit('disconnect');
+        socket.roomid=roomid;
+        room[socket.username]=socket.roomid;
+        Userlist[socket.username]=socket.username;
+        socket.join(roomid);
+        socket.emit('notify','You have joined chatroom ' +roomid);
+        socket.broadcast.to(socket.roomid).emit('notify',socket.username+' have connected!');
+        var local={};
+        for(people in Userlist){
+            if(room[people]==socket.roomid)  local[people]=people;
+        }
+        io.to(socket.roomid).emit('update',local);
+        socket.emit('select',Userlist,socket.username);
+    })
     socket.on('disconnect',function(){
         delete Userlist[socket.username];
-        io.emit('leave',socket.username+' has left');
+        io.to(socket.roomid).emit('leave',socket.username+' has left');
         var local={};
         for(people in Userlist){
             if(room[people]==socket.roomid)  local[people]=people;
@@ -52,9 +55,10 @@ io.on('connection',function(socket){
         io.to(socket.roomid).emit('update',local);
     })
     socket.on('private',function(data,id){
-        var text='From '+socket.username;
-        socket.emit('post',text,data);
-        io.to(ID[id]).emit('post',text,data);
+        var receive='To '+id;
+        var sender ='From '+socket.username;
+        socket.emit('post',receive,data);
+        io.to(ID[id]).emit('post',sender,data);
     })
 })
 http.listen(3000,function(){
