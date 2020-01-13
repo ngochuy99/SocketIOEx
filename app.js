9ab0943875ac14adf0d9248a774d0129a365a5dd
Ngochuy <ngngochuy1107@gmail.com>
2020-01-13 16:21:23 +0700

    Replace css

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express=require('express');
 var app=express();
 var http=require('http').createServer(app);
 var io= require('socket.io')(http);
+app.use(express.static('public'));
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
